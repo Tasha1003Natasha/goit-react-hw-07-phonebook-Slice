380baae7abf60031c9baa0e452571f5addb36ddf
Tasha1003Natasha <0955712824n@gmail.com>
2022-08-10 16:46:37 +0300

    Шаблон

---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,49 @@
+import styles from '../ContactList/ContactList.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchDeleteContact } from 'redux/contacts/contacts-operations';
+// import { useEffect } from 'react';
+// import { fetchContacts } from 'redux/contacts/contacts-operations';
+
+export const ContactList = () => {
+  const filter = useSelector(state => state.contacts.filter);
+  const items = useSelector(state => state.contacts.items);
+  console.log(items);
+  const dispatch = useDispatch();
+
+  ///////////////////////
+
+  // useEffect(() => {
+  //   dispatch(fetchContacts());
+  // }, [dispatch]);
+
+  /////////////////////////
+
+  const handleDelete = id => dispatch(fetchDeleteContact(id));
+
+  const getSubmitContacts = (items, filter) => {
+    return items.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  const contactList = getSubmitContacts(items, filter);
+
+  return (
+    <ul className={styles.contact__list}>
+      {contactList.map(({ id, name, number }) => {
+        return (
+          <li className={styles.contact__item} key={id}>
+            {name} : {number}
+            <button
+              onClick={() => handleDelete(id)}
+              type="button"
+              className={styles.contact__btn}
+            >
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
